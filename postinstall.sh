dc4a109a138f45d0211be05d22d743958eea40ca
Sergey Dmitriev <serge.dmitriev@gmail.com>
2016-02-01 16:39:50 +0700

    fix

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 sudo mkdir -p -m 0777 /etc/viola-cdr && \
-sudo chmod 0777 /etc/viola-cdr && \
-sudo cp ./config.js /etc/viola-cdr/config.js
+sudo cp -f config.js /etc/viola-cdr/config.js
